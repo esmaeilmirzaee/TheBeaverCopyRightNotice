5704e7996319c6b89aa1c6adf9b6a86738be465c
Esmaeil MIRZAEE <esmaeilmirzaee@gmail.com>
2020-02-11 15:57:17 -0500

    revision html safe

--- lib/TheBeaverCopyRightNotice/renderer.rb
+++ lib/TheBeaverCopyRightNotice/renderer.rb
@@ -1,7 +1,7 @@
 module TheBeaverCopyRightNotice
   class Renderer
     def self.copyright name, msg
-      "&copy; #{Time.now.year} | #{name.capitalize}&ndash;#{msg}"
+      "&copy; #{Time.now.year} | #{name.capitalize}&ndash;#{msg}".html_safe
     end
   end
 end
