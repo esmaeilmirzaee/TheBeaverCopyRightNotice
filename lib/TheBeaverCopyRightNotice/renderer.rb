13d40970fa510af0aa0aabf75f9e96cdc0b9e0ec
Esmaeil MIRZAEE <esmaeilmirzaee@gmail.com>
2020-02-12 13:28:59 -0500

    making the text bold

--- lib/TheBeaverCopyRightNotice/renderer.rb
+++ lib/TheBeaverCopyRightNotice/renderer.rb
@@ -1,7 +1,7 @@
 module TheBeaverCopyRightNotice
   class Renderer
     def self.copyright name, msg
-      "&copy; #{Time.now.year} | #{name.capitalize}&ndash;#{msg}".html_safe
+      "&copy; #{Time.now.year} | <b><em>#{name.capitalize}</em></b>&ndash;#{msg}".html_safe
     end
   end
 end
